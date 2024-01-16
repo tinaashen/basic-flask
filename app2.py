0dcd844b991d817739bfa11b51a163c5311d73f8
Tina Shen <shentina18@gmail.com>
2024-01-16 15:16:24 +1100

    added yml and ipynb

---
+++ app2.py
@@ -0,0 +1,14 @@
+from shiny import App, ui
+
+# Part 1: ui ----
+app_ui = ui.page_fluid(
+    "Hello, world!",
+)
+
+# Part 2: server ----
+def server(input, output, session):
+    ...
+
+# Combine into a shiny app.
+# Note that the variable must be "app".
+app = App(app_ui, server)
